399b75a9e7d039c8aa194c96cb230558131a1667
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-12 19:23:35 +0300

    updated

--- src/store.js
+++ src/store.js
@@ -2,10 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import catsReducer from "./features/cats/catsSlice";
 
+// const store = configureStore({
+//   reducer: {
+//     cats: catsReducer,
+//   },
+// });
+
 const store = configureStore({
   reducer: {
     cats: catsReducer,
-  },
+  }
 });
 
 export default store;
